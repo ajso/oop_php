f2b42169042e2dd38ded0980d904a1106408f275
Andrew <andre.ecurut@gmail.com>
2022-06-13 22:33:30 +0400

    Update functions.php
    new commit

--- app/core/functions.php
+++ app/core/functions.php
@@ -21,3 +21,19 @@ function setValue($key){
     }
     return "";
 }
+
+function secure_random_string($length){
+    //define an empty random string.
+    $random_string = '';
+    //loop to generate the string.
+    for($i = 0; $i<$length; $i++){
+        
+        $number = random_int(0, 999999); //generates a random integer in the range 0 to 999999.
+        //convert the $number from base 10 to base 36.
+        $character = base_convert($number, 10, 36);
+        $random_string .= $character;    
+    }
+    //return the generated random value.
+    return $random_string;
+    
+}
